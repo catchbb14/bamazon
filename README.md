27c513683b50a4461a39100131f69f8f2730d04d
Eric Morrow <ericmorrow14@gmail.com>
2018-05-15 03:00:30 -0400

    Completed assignment and updated README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # bamazon
-CLI Application utilizing MySQL
+
+## What is Bamazon?
+
+Bamazon is a CLI application that does the following:
+    * Displays a table containing the items currently in the MySQL database.
+    * Prompts the user for which item and the quantity of the selected item he/she would like to buy.
+    * Displays the price of the transaction.
+    * Updates the quantities within the database and displays the database containing the updated data.
+
+## Why Bamazon?
+
+Bamazon serves to accomplish the following:
+    * Items are displayed in an easy-to-read table fashion
+    * Only items that are in-stock are displayed in the menu
+    * Once an item is selected, the stock quantity is retrieved and used in validation; therefore, the buyer
+        cannot buy more than what is available.
+
+## How do I use bamazon?
+
+#### Step 1: Install dependencies and run node bamazonCustomer.js
+
+![Step 1](./images/step1.png) 
+
+#### Step 2: Choose an item from the provided list and enter a quantity:
+
+![Step 2 - Invalid](./images/step2err.png)
+
+If the inputed value is not a number, less than 1 or more than the stock quantity, the user is prompted to enter
+a number within the valid range.
+
+![Step 2 - Valid](./images/step2.png)
+
+If the value is correct, the total for the transaction is displayed, as well as the updated inventory.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,23 +5,76 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '',
+    password : 'Edogbb14',
     database : 'bamazon'
 });
 var availableItems = [];
 
 function refreshTable() {
     availableItems = [];
-    connection.connect();
+
     
     connection.query('SELECT * FROM products', function (error, results, fields) {
     if (error) throw error;
     displayItems(results);
     });
-    connection.end();
 }
 
-function promptUser() {
+function updateQuantity(id, number, refreshTable) {
+    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [number, id],
+    function( error, results) {
+        if(error) throw error;
+        refreshTable();
+        connection.end();
+    }
+)
+}
+
+function getMaxQuantity(id) {
+    var max;
+    connection.query('SELECT * FROM products WHERE item_id = ?', [id],
+            function( error, results, fields) {
+                if(error) throw error;
+                promptQuantity(results[0]);
+            }
+    )
+}
+
+function promptQuantity(item) {
+    var max = item.stock_quantity;
+    var price = item.price;
+    var item_id = item.item_id;
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "quantity",
+            message: `How many would you like to buy?`,
+            validate: function (input) {
+                var done = this.async();
+                setTimeout( function() {
+                    if( isNaN(input) || parseInt(input) < 1 || parseInt(input) > max) {
+                        done(`Please provide a number between 1 and ${max}`);
+                        return;
+                    }
+                    done(null, true);
+                }, 1000);
+                
+            }
+        }
+    ]).then( function(answer) {
+        updateQuantity(item_id, max - answer.quantity, refreshTable);
+        console.log(
+                `Your transaction is being processed...
+                The amount of $${(answer.quantity * price).toFixed(2)}
+                has been charged to your account... 
+                Thank you for shopping at Bamazon!
+                `);
+        
+    })
+}
+
+function promptItem() {
     var availableChoices = [];
     availableItems.forEach( function(item) {
         availableChoices.push({
@@ -39,7 +92,7 @@ function promptUser() {
         }
     ]).then( function(answer) {
         var item_id = answer['itemChosen'];
-        
+        getMaxQuantity(item_id);
     })
 }
 
@@ -52,17 +105,20 @@ function displayItems(productList) {
     }); 
 
     productList.forEach( function(item) {
-        table.push([item.item_id, item.product_name, item.department_name, item.price, item.stock_quantity]);
+        table.push([item.item_id, item.product_name, item.department_name, item.price.toFixed(2), item.stock_quantity]);
         if(item.stock_quantity > 0) {
             availableItems.push(item);
         }
     })
     
     console.log(`Current Inventory\n${table.toString()}\n`);
-    promptUser();
+    
 }
 
-
+connection.connect();
 refreshTable();
+setTimeout( function() {
+    promptItem();
+},1000);
 
  
